a6bab02e95c00138e04430df20eb0ffd6eb5122f
samfan01 <dsamfan@gmail.com>
2021-03-08 19:59:38 +0300

    created the base html file

--- app/views.py
+++ app/views.py
@@ -9,4 +9,6 @@ def index():
     view root page function that returns the index page and its
     data
     '''
-    return render_template('index.html')
+    title = 'Home - Welcome to Douglas Samphan Blog Spot.'
+    
+    return render_template('index.html',title = title)
