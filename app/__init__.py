cb9aa27c026123b620706e6cb37a31bcee89eed3
samfan01 <dsamfan@gmail.com>
2021-03-09 12:33:39 +0300

    created the main folder for defining my blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,19 @@
 from flask import Flask 
-from .config import DevConfig
+from config import config_options
 from flask_bootstrap import Bootstrap 
 
-#Initializing application
-app = Flask(__name__)
+bootstrap = Bootstrap()
 
-#Setting up configuration
-app.config.from_object(DevConfig)
+def create_app(config_name):
+    
+    app = Flask(__name__)
+    
+    #Creating the app configuration
+    app.config.from_object(config_options[config_name])
 
-#Initializing Flask Extensions
-bootstrap = Bootstrap(app)
+    #Initializing Flask Extensions
+    bootstrap = Bootstrap(app)
 
-from app import views
+    #will add the Views and forms
+    
+    return app
