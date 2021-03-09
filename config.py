bd60dd95f56a95bb3b7e050aec5ba76a62442104
samfan01 <dsamfan@gmail.com>
2021-03-09 23:37:04 +0300

    created the tests folder to carry my model test files

--- config.py
+++ config.py
@@ -3,15 +3,17 @@ import os
 class Config:
  
     SECRET_KEY =os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:access@localhost/watchlist'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Samfan01@localhost/blog'
 
 class ProdConfig(Config):
     
     pass
 class DevConfig(Config):
- 
-    
     DEBUG = True
+    
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+    
     
 config_options = {
 'development':DevConfig,
